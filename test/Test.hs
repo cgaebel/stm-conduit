01ffcdac2a795fff1cb8939b77f96c513c1d3255
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-05-13 13:00:42 +0400

    add tests for closable queues

--- test/Test.hs
+++ test/Test.hs
@@ -24,6 +24,7 @@ tests = [
         testGroup "Behaves to spec" [
                   testCase "simpleList using TMChan" test_simpleList
                 , testCase "simpleList using TQueue" test_simpleQueue
+                , testCase "simpleList using TMQueue" test_simpleMQueue
             ],
         testGroup "Bug fixes" [
                 testCase "multipleWriters" test_multipleWriters
@@ -45,6 +46,16 @@ test_simpleQueue = do q <- atomically $ newTQueue
                       assertEqual "for the numbers [1..10000]," testList lst'
     where
         testList = [1..10000]
+
+test_simpleMQueue = do q <- atomically $ newTMQueue
+                       forkIO . runResourceT $ sourceList testList $$ sinkTMQueue q
+                       lst' <- runResourceT $ sourceTMQueue q $$ consume
+                       assertEqual "for the numbers [1..10000]," testList lst'
+                       closed <- atomically $ isClosedTMQueue q
+                       assertBool "channel is closed after running" closed
+    where
+        testList = [1..10000]
+
 test_multipleWriters = do ms <- runResourceT $ mergeSources [ sourceList ([1..10]::[Integer])
                                                             , sourceList ([11..20]::[Integer])
                                                             ] 3
