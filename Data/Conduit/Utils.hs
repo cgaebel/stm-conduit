23ae4c99d25606d3a30a2c8965341aa28023b8ef
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-03-09 01:07:18 +0400

    Improve documentation

--- Data/Conduit/Utils.hs
+++ Data/Conduit/Utils.hs
@@ -1,9 +1,51 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-| 
+This module provide different utility functions that allow to use safe higher 
+level usage.
+
+Conduit pairs allow creation of an internal datastructure that acts as a bridge,
+and provides input and output conduits. The structure itself is hidden
+internally and can't be used directly, this provide an additional
+safeness.
+
+In order to create a bridge from your own datastructures you need to do
+the following:
+
+  * Make it an instance of 'UnboundedStream' or 'BoundedStream'
+  depending on it's properties:
+
+> instance BoundedStream (Proxy2 TBMQueue) TBMQueue where
+>  mkBStream _ i = atomically $ newTBMQueue i
+
+  * Add 'IsConduit' instance.
+
+>  instance MonadIO m => IsConduit m TBMQueue where
+>    mkSource = sourceTBMQueue
+>    mkSink   = flip sinkTBMQueue True
+
+  * Use "pair" or "pairBounded" to create a bridge. Because bridge data structure 
+    is hidden and not seen in parameters, we need proxy type to help compiler to 
+    choose type, we use "Proxy2" for that.
+
+>  pairTBMQueue = pairBounded (proxy2 :: Proxy2 TBMQueue a)
+
+  * Now we can create a pair of conduits:
+
+> (src, snk) <- pairTBMQueue 32
+> Control.Concurrent.Async.concurrently (sender src) (receviver snk)
+
+  As channel is not visible we can close it or send additional messages bypassing
+  conduit code.
+
+This package provides predefined pairs for all STM types that are used
+in the package. 
+
+-}
 module Data.Conduit.Utils
   ( 
-  -- * Build constructs
+  -- * Conduit pairs
   -- ** Low level functions
     pairBounded   -- MonadIO m => m (Source m a, Sink m a ())
   , pair          -- MonadIO m => Int -> m (Source m a, Sink m a ()) 
@@ -15,6 +57,7 @@ module Data.Conduit.Utils
   , Proxy2
   , proxy2        -- Proxy a b
   -- * Specialized functions
+  -- $flist
   , pairTQueue    -- MonadIO m => m (Source m a, Sink a m ())
   , pairTMQueue   -- MonadIO m => m (Source m a, Sink a m ())
   , pairTMChan    -- MonadIO m => m (Source m a, Sink a m ())
@@ -46,6 +89,9 @@ proxy2 :: Proxy2 a b
 proxy2 = Proxy2
 
 -- | Class for structures that can handle unbounded stream of values.
+-- Such streams break conduit assumptions that constant memory will be
+-- used, because if receiver is slower then sender than values will 
+-- be accumulated.
 class UnboundedStream i o | i -> o where
   mkUStream :: i a -> IO (o a)
 
@@ -62,16 +108,18 @@ class MonadIO m => IsConduit m (x :: * -> *) where
   mkSink   :: x a -> Sink a m ()
   mkSource :: x a -> Source m a
 
+-- | Create bounded conduit pair, see "BoundedStream" class description.
 pairBounded :: (MonadIO m, IsConduit m o, BoundedStream i o)
-            => i a
-            -> Int
+            => i a    -- ^ Type description.
+            -> Int    -- ^ Conduit size.
             -> m (Source m a, Sink a m ())
 pairBounded p s = do
   q <- liftIO $ mkBStream p s
   return (mkSource q, mkSink q)
 
+-- | Create unbounded pair, see "UnboundedStream" class description.
 pair :: (MonadIO m, IsConduit m o, UnboundedStream i o)
-     => i a
+     => i a   -- ^ Type description.
      -> m (Source m a, Sink a m ())
 pair p = do
   q <- liftIO $ mkUStream p
@@ -125,6 +173,12 @@ instance MonadIO m => IsConduit m TMChan where
 -------------------------------------------------------------------------------
 -- Specialized functions
 -------------------------------------------------------------------------------
+-- $flist
+-- List of specialized functions, that can create a bridges over STM types,
+-- where *B* stands for bounded *M* stands for closable. If data structure
+-- is not closable then there is no way to notify receiver side that bridge
+-- is closed, so it's possible to use it only in infinite streams of when
+-- some other mechanism of notification is used.
 pairTQueue, pairTMQueue, pairTMChan :: MonadIO m => m (Source m a, Sink a m ())
 pairTQueue   = pair (proxy2 :: Proxy2 TQueue a)
 pairTMQueue  = pair (proxy2 :: Proxy2 TMQueue a)
